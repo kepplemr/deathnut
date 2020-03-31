c61bb2cc9d18d57ac7aff2cea9abeffd23ff2d6f
Michael R Kepple <kepplemr@gmail.com>
2020-03-31 06:08:15 -0700

    remove old tests

--- test/e2e_tests/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PORT=82
-id=$(curl -X POST "http://localhost:$PORT/recipe" -H "content-type: application/json" -d '{"title": "Michael Cold Brew", "ingredients": ["Water", "Coffee"]}' 2> /dev/null | jq -r '.id')
-echo "Created recipe id: $id"
-curl -X GET "http://localhost:$PORT/recipe/$id" -H "content-type: application/json"
-echo
-curl -X PATCH "http://localhost:$PORT/recipe/$id" -H "content-type: application/json" -d '{"ingredients": ["Water", "Coffee", "Spices"]}'
