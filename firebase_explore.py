0759d182a413bc4e29b8882ac64c0313b47aca89
Michael R Kepple <kepplemr@gmail.com>
2020-04-16 04:27:10 -0700

    firebase jwt working!

--- firebase_explore.py
+++ firebase_explore.py
@@ -1,26 +1,24 @@
-import firebase_admin
+"""Dimsum CE e2e auth/deathnut test script"""
 import json
+import firebase_admin
 import requests
 from firebase_admin import credentials
 from firebase_admin import auth
 
-def create_token_with_claims():
-    cred = credentials.Certificate('./keys/gcloud-integration.json')
-    default_app = firebase_admin.initialize_app(cred)
-    # [START create_token_with_claims]
-    uid = 'some-uid'
-    email = 'michael@aol.com'
-    additional_claims = {
-        'premiumAccount': True
-    }
+# make sure service account has 'firebaseauth.users.[create,get]'
+SERVICE_ACCOUNT_KEY_LOCATION = './keys/gcloud-integration.json'
+# make sure the key used has 'Identity Toolkit API' access. Or sign in with email/pw.
+FIREBASE_WEB_API_KEY = 'AIzaSyDjGqnElsoDTV6BjMdmRGjIk8f04aUQMXU'
 
-    custom_token = auth.create_custom_token(uid, additional_claims)
-    # [END create_token_with_claims]
-    firebase_admin.delete_app(default_app)
-    return custom_token
+def get_id_token_for_user_email(email):
+    user_id = auth.get_user_by_email(email).uid
+    user_custom_token = auth.create_custom_token(user_id).decode()
+    idtk_url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key='
+    payload = {"token": user_custom_token, "returnSecureToken": True}
+    return requests.post(''.join([idtk_url, FIREBASE_WEB_API_KEY]), data=payload).json().get('idToken')
 
 def make_jwt_request(method, url, signed_jwt, data=None, extra_header=None):
-    headers = {"Authorization": "Bearer {}".format(signed_jwt.decode("utf-8")),
+    headers = {"Authorization": "Bearer {}".format(signed_jwt),
         "content-type": "application/json"}
     if extra_header:
         headers.update(extra_header)
@@ -28,9 +26,13 @@ def make_jwt_request(method, url, signed_jwt, data=None, extra_header=None):
     print(str(response.text))
     return response
 
-token = create_token_with_claims()
-# TODO replace url with CE
+def main():
+    firebase_admin.initialize_app(credentials.Certificate(SERVICE_ACCOUNT_KEY_LOCATION))
+    # if user does not yet exist, first run auth.create_user(email=<>, password=<>)
+    michael_jwt = get_id_token_for_user_email('michael@test.com')
+    jennifer_jwt = get_id_token_for_user_email('jennifer@test.com')
 
-recipe = json.loads(make_jwt_request(requests.get, "http://localhost:{}/recipe/{}".format(port, spaghetti_id), jwt).text)
+#url = 'https://dimsum-jwt.endpoints.wellio-integration.cloud.goog/api/edited_recipes'
 
-print(str(token))
+if __name__ == "__main__":
+    main()
