b0af0a666844624b3251a0b44838d3adf426268f
Michael R Kepple <kepplemr@gmail.com>
2020-04-27 10:29:17 -0700

    bump to 1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="deathnut",
-    version="0.1",
+    version="1.0",
     description="Simple redis-based authorization library",
     install_requires=["redis==3.3.11"],
     packages=find_packages(),
